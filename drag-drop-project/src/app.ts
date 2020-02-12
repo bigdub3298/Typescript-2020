49ed9248844bf166d26f6951f43732c07ddabcc2
bigdub3298 <wesleyaustin33@gmail.com>
2020-02-11 19:03:47 -0700

    rendered form element to screen

--- drag-drop-project/src/app.ts
+++ drag-drop-project/src/app.ts
@@ -1,1 +1,26 @@
-// Code goes here!
+class ProjectInput {
+  templateElement: HTMLTemplateElement;
+  hostElement: HTMLDivElement;
+  element: HTMLFormElement;
+
+  constructor() {
+    this.templateElement = document.querySelector(
+      "#project-input"
+    )! as HTMLTemplateElement;
+    this.hostElement = document.querySelector("#app")! as HTMLDivElement;
+
+    const importedNode = document.importNode(
+      this.templateElement.content,
+      true
+    );
+    this.element = importedNode.firstElementChild as HTMLFormElement;
+    this.element.id = "user-input";
+    this.attach();
+  }
+
+  private attach() {
+    this.hostElement.insertAdjacentElement("afterbegin", this.element);
+  }
+}
+
+const prjInput = new ProjectInput();
